117179fe843c96a2387cb0b79778769364ea1aec
Zach Neill <zacharyneill@gmail.com>
2023-01-27 21:06:11 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'too-loudifier';
+export class AppComponent implements OnInit {
+  context: AudioContext = new AudioContext
+  gainNode: GainNode = this.context.createGain()
+
+  ngOnInit() {
+    this.gainNode.connect(this.context.destination)
+  }
+
+  changeVolume(newVolume: number): void {
+    this.gainNode.gain.setValueAtTime(newVolume, this.context.currentTime);
+  }
+
 }
